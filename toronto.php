4669a6dce365c9e5b55fef382ee4cdafd369fb30
Alan Dixon <alan.g.dixon@gmail.com>
2022-09-09 18:24:55 +0000

    initial scaffolding from civix

---
+++ toronto.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'toronto.civix.php';
+// phpcs:disable
+use CRM_Toronto_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function toronto_civicrm_config(&$config) {
+  _toronto_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function toronto_civicrm_install(): void {
+  _toronto_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function toronto_civicrm_postInstall(): void {
+  _toronto_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function toronto_civicrm_uninstall(): void {
+  _toronto_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function toronto_civicrm_enable(): void {
+  _toronto_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function toronto_civicrm_disable(): void {
+  _toronto_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function toronto_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _toronto_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function toronto_civicrm_entityTypes(&$entityTypes): void {
+  _toronto_civix_civicrm_entityTypes($entityTypes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function toronto_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function toronto_civicrm_navigationMenu(&$menu): void {
+//  _toronto_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _toronto_civix_navigationMenu($menu);
+//}
---
+++ toronto.theme.php
@@ -0,0 +1,14 @@
+<?php
+// This file declares a CSS theme for CiviCRM.
+
+return [
+  'name' => 'toronto',
+  'title' => 'toronto theme',
+  'prefix' => null,
+  'url_callback' => '\\Civi\\Core\\Themes\\Resolvers::simple',
+  'search_order' => [
+    'toronto',
+    '_fallback_',
+  ],
+  'excludes' => [],
+];
