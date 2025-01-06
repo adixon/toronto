01af3f41c46a91b61376330f2f72e506b3b38528
Alan Dixon <alan.g.dixon@gmail.com>
2025-01-06 16:36:19 +0000

    civix upgrade

--- toronto.php
+++ toronto.php
@@ -24,59 +24,12 @@ function toronto_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function toronto_civicrm_postInstall(): void {
-  _toronto_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function toronto_civicrm_uninstall(): void {
-  _toronto_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function toronto_civicrm_enable(): void {
   _toronto_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function toronto_civicrm_disable(): void {
-  _toronto_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function toronto_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _toronto_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function toronto_civicrm_entityTypes(&$entityTypes): void {
-  _toronto_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
